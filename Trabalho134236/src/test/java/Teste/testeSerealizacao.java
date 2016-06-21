ffc55c21fb824842d748d6cd0dc0796c10476099
Edson <edsonrcb@hotmail.com>
2016-06-21 20:26:59 -0300

    teste unitario classe serealizacao

---
+++ Trabalho134236/src/test/java/Teste/testeSerealizacao.java
@@ -0,0 +1,27 @@
+package Teste;
+
+
+import static junit.framework.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import dao.Serealizacao;
+
+public class testeSerealizacao {
+
+	@Before
+	public void setUp() throws Exception {
+		Serealizacao s = new Serealizacao();
+	}
+
+	@Test
+	public final void testGravarProduto() {
+		assertEquals(expected, actual);
+	}
+	
+
+	@Test
+	public final void testLerProduto() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
