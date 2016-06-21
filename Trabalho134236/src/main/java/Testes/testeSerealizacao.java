59de0d59b15ab7bbf2eca7f0517dc0f0c4563371
Edson <edsonrcb@hotmail.com>
2016-06-21 19:37:41 -0300

    jUnitcriado

---
+++ Trabalho134236/src/main/java/Testes/testeSerealizacao.java
@@ -0,0 +1,19 @@
+package Testes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class testeSerealizacao {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+}
